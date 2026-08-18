4f46141437f96773d0aa488027974bf8eb6ef8c7
KryozenStudio <rohitsinghp95@gmail.com>
2026-08-18 09:59:25 +0530

    Add video player test project

--- config/site.config.js
+++ config/site.config.js
@@ -249,5 +249,17 @@ window.SITE_CONFIG = {
   //   software: ["Node Video"],
   //   date: "2026-08-07",
   // },
-  projects: [],
+  projects: [
+    {
+      id: "testing-video-player",
+      title: "Testing Video Player",
+      category: "Anime / AMV",
+      thumbnail: "",
+      video: "assets/videos/testing-video-player.mp4",
+      description: "",
+      software: ["Node Video"],
+      date: "2026-08-18",
+      featured: false
+    }
+  ],
 };
